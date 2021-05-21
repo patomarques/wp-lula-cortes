9d95a2ae613ffd8646aff1ce0b9963b3f52160bd
Pato Marques <patoxmarques@gmail.com>
2021-05-21 11:25:10 -0300

    header menu overlay has updated

--- assets/js/lulacortes.js
+++ assets/js/lulacortes.js
@@ -1,7 +1,7 @@
-(function($) {
-    $(document).ready(function(){
-        $('#toggle').click(function () {
-            $(this).toggleClass('active');
+(function ($) {
+    $(document).ready(function () {
+        $('.toggle').on('click', function () {
+            $('.toggle').toggleClass('active');
             $('#overlay').toggleClass('open');
         });
 
@@ -10,8 +10,24 @@
         //     slidesToShow: 1,
         //     slidesToScroll: 1
         // });
+
+        var $animation_elements = $('.slide');
+        var $window = $(window);
+
+        function check_if_in_view() {
+            var window_height = $window.height();
+            var window_top_position = $window.scrollTop();
+            var window_bottom_position = (window_top_position + window_height);
+            $.each($animation_elements, function () {
+                var $element = $(this);
+                var element_height = $element.outerHeight();
+                var element_top_position = $element.offset().top;
+                var element_bottom_position = (element_top_position + element_height);
+                if ((element_bottom_position >= window_top_position) &&
+                    (element_top_position <= window_bottom_position)) $element.addClass('hasSlid');
+                else $element.removeClass('hasSlid');
+            });
+        }
+        $window.on('scroll resize', check_if_in_view);
     });
 })(jQuery);
-
-
-
