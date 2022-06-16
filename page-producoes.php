1bc2d1a9b47f84ee79e44ef2505621485b5e1c79
Dayvson Marques <dayvson.b.marques@accenture.com>
2022-06-16 20:28:00 -0300

    paginas de produções, artes visuais e footer foram melhorados

--- page-producoes.php
+++ page-producoes.php
@@ -3,24 +3,7 @@
 global $post;
 $post_slug = $post->post_name;
 
-$argsLivros = array('post_type' => 'livros', 'orderby' => 'Ordem', 'order' => 'ASC', 'posts_per_page' => -1);
-$queryLivros = new WP_Query($argsLivros);
-
-$argsZines = array('post_type' => 'zines', 'orderby' => 'Ordem', 'order' => 'ASC', 'posts_per_page' => -1);
-$queryZines = new WP_Query($argsZines);
-
-$argsJogos = array('post_type' => 'jogos', 'orderby' => 'Ordem', 'order' => 'ASC', 'posts_per_page' => -1);
-$queryJogos = new WP_Query($argsJogos);
-
-$argsAudioBooks = array('post_type' => 'audiobooks', 'orderby' => 'Ordem', 'order' => 'ASC', 'posts_per_page' => -1);
-$queryAudioBooks = new WP_Query($argsAudioBooks);
-
-$argsPdfs = array('post_type' => 'pdfs', 'orderby' => 'Ordem', 'order' => 'ASC', 'posts_per_page' => -1);
-$queryPdfs = new WP_Query($argsPdfs);
-
 ?>
-
-
 <section id="page-content" class="container content-sections">
   <div class="row pt-5 mb-5">
     <div class="col-12 mt-5 pt-3">
@@ -34,78 +17,13 @@ $queryPdfs = new WP_Query($argsPdfs);
   </div>
 </section>
 
-<div class="container-full content-slick-squares mt-5 mb-5">
-  <div id="slick-livros" class="slick-squares mb-5">
+<?php get_template_part('template-part/livros'); ?>
 
-    <?php while ($queryLivros->have_posts()) : $queryLivros->the_post(); ?>
+<?php get_template_part('template-part/zines'); ?>
 
-    <?php
-      $thumbs = get_the_post_thumbnail_url();
+<?php get_template_part('template-part/audiobooks'); ?>
 
-      if($thumbs == ''){
-        $thumbs = get_site_url() . '/wp-content/uploads/2022/06/no-image.jpg';
-      }
-    ?>
-
-    <div class="slick-squares__item">
-      <div class="slick-squares__item__text">
-        <a href="/" class="slick-squares__link">
-          <p class="slick-squares__title"><?= get_the_title() ?></p>
-          <!-- <p class="slick-squares__subtitle">2000</p> -->
-        </a>
-      </div>
-      <a href="/"
-        target="_black" rel="noopener" title="<?= get_the_title() ?>"
-        class="slick-squares__box-image slick-squares__link"
-        style="background-image: url(<?=  $thumbs ?>)">
-        <div class="slick-squares__box-image__spacer"></div>
-      </a>
-    </div>
-
-    <?php
-      endwhile;
-      wp_reset_postdata();
-    ?>
-
-  </div>
-</div>
-
-<div class="container-full content-slick-squares mt-5 mb-5">
-  <div id="slick-zines" class="slick-squares mb-5">
-
-    <?php while ($queryZines->have_posts()) : $queryZines->the_post(); ?>
-
-    <?php
-      $thumbs = get_the_post_thumbnail_url();
-
-      if($thumbs == ''){
-        $thumbs = get_site_url() . '/wp-content/uploads/2022/06/no-image.jpg';
-      }
-    ?>
-
-    <div class="slick-squares__item">
-      <div class="slick-squares__item__text">
-        <a href="/" class="slick-squares__link">
-          <p class="slick-squares__title"><?= get_the_title() ?></p>
-          <!-- <p class="slick-squares__subtitle">2000</p> -->
-        </a>
-      </div>
-      <a href="/"
-        target="_black" rel="noopener" title="<?= get_the_title() ?>"
-        class="slick-squares__box-image slick-squares__link"
-        style="background-image: url(<?=  $thumbs ?>)">
-        <div class="slick-squares__box-image__spacer"></div>
-      </a>
-    </div>
-
-    <?php
-      endwhile;
-      wp_reset_postdata();
-    ?>
-
-  </div>
-</div>
-
+<?php get_template_part('template-part/pdfs'); ?>
 
 <script>
 	jQuery(document).ready(function() {
