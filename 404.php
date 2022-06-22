b3537f0a3460edb2ed9689be4e3efea35e3711d1
Dayvson Marques <dayvson.b.marques@accenture.com>
2022-06-22 12:26:07 -0300

    404 page not found was developed

--- 404.php
+++ 404.php
@@ -2,10 +2,25 @@
   get_header();
 ?>
 
-<div id="post-0" class="content error404 not-found">
-	<div class="entry-content text-center">
-		<h2 class="title-not-found">Página não encontrada!</h2>
-	</div>
+<div id="page-content" class="container content-sections mt-5 pt-2">
+  <div class="row mt-5 pt-5">
+    <div class="col-sm-12 col-md-6 offset-md-3 text-center">
+    <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/404.png"
+      alt="Erro 404 - Página Não encontrada"
+      title="Erro 404 - Página Não encontrada"
+      class="img-fluid m-auto d-block">
+
+  		<h2 class="title-not-found">Página não encontrada!</h2>
+
+      <a href="/" class="btn btn-link mt-5" rel="noopener noreferrer"><< Voltar ao Início</a>
+    </div>
+  </div>
 </div>
+
+<script>
+	jQuery(document).ready(function() {
+		jQuery('.menu-main').addClass('shrink menu-main--dark');
+	});
+</script>
 
 <?php get_footer(); ?>
