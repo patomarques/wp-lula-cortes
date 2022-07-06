8d93c7766c1ed9f8fc657b223a88ac37b5d0261d
Pato Marques <patoxmarques@gmail.com>
2022-07-06 08:58:04 -0300

    banner mobile fix

--- template-part/banner-simple.php
+++ template-part/banner-simple.php
@@ -1,28 +1,27 @@
 <?php
-
-    //get all banners
     $args = array( 'post_type' => 'banners', 'posts_per_page' => 10 );
     $bannerQuery = new WP_Query( $args );
-
 ?>
 
-<div id="slider-banners2" class="container-full banner-main hidden">
+<div id="slider-banners" class="container-full slider-banner header-image d-none d-xs-none d-sm-none d-md-none d-lg-block">
     <?php while ( $bannerQuery->have_posts() ) : $bannerQuery->the_post(); ?>
-    <div class="banner-main__item" data-image="<?= get_the_post_thumbnail_url() ?>">
-
-    </div>
-    <?php
-        endwhile;
-        wp_reset_postdata();
-    ?>
-</div>
-
-<div id="slider-banners" class="container-full slider-banner header-image">
-    <?php while ( $bannerQuery->have_posts() ) : $bannerQuery->the_post(); ?>
-        <div class="hero slider-banner__item" style="background-image: url(<?= get_the_post_thumbnail_url( ) ?>)">
+    
+        <div class="hero slider-banner__item " style="background-image: url(<?= get_the_post_thumbnail_url() ?>)">
         </div>
     <?php
         endwhile;
         wp_reset_postdata();
     ?>
 </div>
+
+<div id="slider-banners-mobile" class="container-full slider-banner header-image d-lg-none">
+    <?php while ( $bannerQuery->have_posts() ) : $bannerQuery->the_post(); ?>
+    
+        <div class="hero slider-banner__item" style="background-image: url(<?= get_post_field('mobile-image'); ?>)">
+        </div>
+    <?php
+        endwhile;
+        wp_reset_postdata();
+    ?>
+</div>
+
